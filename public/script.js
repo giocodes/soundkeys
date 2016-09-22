1bc8fe5d767e179764f67264ba8005cae91ea881
[Gio] <[sigronline@gmail.com]>
2016-09-22 14:54:26 -0500

    now to three.js

--- public/script.js
+++ public/script.js
@@ -1,41 +1,61 @@
-
-function alerta(){
-alert("Hello");	
+function alerta() {
+    alert("Hello");
 }
 
-var sound = new Howl({
-  src: ['kick.wav'],
-  loop: true
+var kick = new Howl({
+    src: ['kick.wav'],
+    loop: true,
+    speed: 2
 });
 
-$( '#status' ).click(function() {
-  $( this ).text( 'Changed' );
-  // alerta();
+var bass = new Howl({
+    src: ['bass.wav'],
+    loop: true
 });
 
-var playing = false;
-$( "body" ).keydown(function(event) {
-  $( '#status' ).text( event.which );
-  // console.log( "Handler for .keypress() called." );
-  // alerta();
-  console.log('playing is ', playing);
-  if(!playing){
-  	sound.volume(1);
-  	sound.play();
-  	playing = true;
-  }
-sound.on('end', () => console.log('Boom!'));
+$('#status').click(function() {
+    $(this).text('Changed');
+    // alerta();
+});
+
+var playingKick = false;
+var playingBass = false;
+$("body").keydown(function(event) {
+    $('#status').text(event.which);
+    console.log('playing is ', playingKick, playingBass);
+    if (event.which === 74) {
+        if (!playingKick) {
+            kick.volume(1);
+            kick.play();
+            playingKick = true;
+        }
+    }
+    if (event.which === 70) {
+        if (!playingBass) {
+            bass.volume(1);
+            bass.play();
+            playingBass = true;
+        }
+    }
+
+    kick.on('end', () => console.log('Psss!'));
+    bass.on('end', () => console.log('Boom!'));
 
 });
 
-$( "body" ).keyup(function(event) {
-  $( '#status' ).text( event.which );
-  // console.log( "Handler for .keypress() called." );
-  // alerta();
-  playing = false;
-  sound.fade(1,0,200);
-  sound.on('fade', () => sound.stop());
+$("body").keyup(function(event) {
+    $('#status').text(event.which);
+    // console.log( "Handler for .keypress() called." );
+    // alerta();
+    if (event.which === 74) {
+      playingKick = false;
+      kick.stop();
+      // kick.fade(1, 0, 200);
+      // kick.on('fade', () => kick.stop());
+    }
+    if (event.which === 70) {
+      playingBass = false;
+      bass.fade(1, 0, 200);
+      bass.on('fade', () => bass.stop());
+    }
 });
-
-
-
